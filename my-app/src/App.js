f68c265a672df29b6fc1120bc1c474b88eb846b6
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-14 10:35:18 -0600

    Added footer and links to footer, hosting on vercal instead

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,37 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello World!</h1>
+        </header>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/whaleism"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jenny Lee
+          </a>
+          , on{" "}
+          <a
+            href="https://github.com/whaleism/react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          , hosted by{" "}
+          <a
+            href="https://roaring-meerkat-514551.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vercel
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
